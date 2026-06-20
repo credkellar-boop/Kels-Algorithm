29ecc98066b9d3841c41c2ffd16aa2d8f9cd7e0e
Darion Kellar <credkellar@gmail.com>
2026-06-20 04:34:55 -0700

    Add IsogenyEngine class with initial structure

---
+++ src/crypto/isogeny.py
@@ -0,0 +1,8 @@
+class IsogenyEngine:
+    def __init__(self):
+        self.curve_state = "EXPERIMENTAL"
+        
+    def walk_graph(self):
+        # Future implementation for pseudo-random walk across elliptic curve graphs
+        pass
+      
